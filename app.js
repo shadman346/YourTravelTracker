e7de16212ddf6c1fd807d86a38e5203b005d11c9
shadman346 <shadmanansari346c@gmail.com>
2021-03-27 05:05:52 +0530

    port resolve

--- app.js
+++ app.js
@@ -136,6 +136,7 @@ app.use((err, req, res, next) => {
 
 //server portttttt
 
-app.listen(3000, () => {
-   console.log('Serving on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+   console.log(`Serving on port ${port}`);
 });
